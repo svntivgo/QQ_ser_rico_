4b90ef422b9be119dd02eaaa49d397c86c334d75
Santiago Barrera <santiago.barrerae@gmail.com>
2021-12-29 22:54:16 -0500

    comments

--- js/modules/start.js
+++ js/modules/start.js
@@ -1,5 +1,8 @@
 import { eraser } from "../models/eraser.js";
-
+/**
+ * Starts the game
+ * @param {function} nextStep just the next after player press CONTINUAR first time
+ */
 export function start (nextStep) {
     eraser()
     nextStep()
