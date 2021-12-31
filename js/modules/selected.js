769bf697a1b24316f699971710cd488e14439114
Santiago Barrera <santiago.barrerae@gmail.com>
2021-12-31 16:12:49 -0500

    Win adding correction and some errors w/ listeners

--- js/modules/selected.js
+++ js/modules/selected.js
@@ -1,25 +1,38 @@
+// import { mainButton } from "../models/button";
+
 /**
  * Changes the color of the selected answer by player
  * CASE/PROBLEM
  */
-export function answerSelection() {
+export function answerSelection(number) {
+let main = document.getElementById('main')
+let validator = number <= 4
 
-    for (let i = 0; i <= 4; i++) {
-        let answersButton = document.getElementById(`answer${i}`)
+    switch (validator) {
+        case false:
+            document.removeEventListener('click', () => {});
+            break;
 
-        if (answersButton !== null) {
-            document.addEventListener('click', (e) => {
-                switch (answersButton.checked) {
-                    case true:
-                        document.getElementById(i - 1).classList.add('selected');
-                        break;
-                    case false:
-                        document.getElementById(i - 1).classList.remove('selected')
-                        break;
-                    default:
-                        break;
+        default:
+            for (let i = 0; i <= 4; i++) {
+                let answersButton = document.getElementById(`answer${i}`)
+
+                if (answersButton !== null) {
+                    main.addEventListener('click', (e) => {
+                        switch (answersButton.checked) {
+                            case true:
+                                document.getElementById(i - 1).classList.add('selected');
+                                break;
+                            case false:
+                                document.getElementById(i - 1).classList.remove('selected')
+                                break;
+                            default:
+                                break;
+                        }
+                    })
                 }
-            })
-        }
+            };
+            break;
     }
+
 }
